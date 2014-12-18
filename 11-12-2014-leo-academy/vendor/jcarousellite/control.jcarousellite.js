dae24d7239b58c35194e3f961032d823f998ee8a
Aleksandr Honcharov <charleksvik@gmail.com>
2014-12-18 11:06:26 +0200

    + 11-12-2014-leo-academy

---
+++ 11-12-2014-leo-academy/vendor/jcarousellite/control.jcarousellite.js
@@ -0,0 +1,5 @@
+$(".reviews_graduates_content_carousel").jCarouselLite({
+    btnNext: ".reviews_graduates_content_carousel_next",
+    btnPrev: ".reviews_graduates_content_carousel_prev",
+    speed: 500            
+});
